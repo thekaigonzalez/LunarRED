843ac2f209857a87d18f6a6d8ea872d2ae324d01
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-17 17:34:27 -0500

    a slightly bigger file, still proud of it tho

---
+++ lunar/testes/asm/actual-hello.asm
@@ -0,0 +1,59 @@
+# actually hello world this time
+
+@M:
+# move 'H' into R1
+  mov R1, 0x48
+  null
+
+# move 'E' into R1
+  mov R1, 0x45
+  null
+
+# move 'L' into R1
+  mov R1, 0x4c
+  null
+
+# move 'L' into R1
+  mov R1, 0x4c
+  null
+
+# move 'O' into R1
+  mov R1, 0x4f
+  null
+
+# move ' ' into R1
+  mov R1, 0x20
+  null
+
+# move 'W' into R1
+  mov R1, 0x57
+  null
+
+# move 'O' into R1
+  mov R1, 0x4f
+  null
+
+# move 'R' into R1
+  mov R1, 0x52
+  null
+
+# move 'L' into R1
+  mov R1, 0x4c
+  null
+
+# move 'D' into R1
+  mov R1, 0x44
+  null
+
+# move '!' into R1
+  mov R1, 0x21
+  null
+
+# move '\n' into R1
+  mov R1, 0x0a
+  null
+
+  each R1
+  null
+
+  halt
