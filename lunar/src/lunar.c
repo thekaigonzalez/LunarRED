d087b8c5163b9dc039450dd12fbb4e0e26e015ab
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-17 17:26:31 -0500

    make the compiler more robust

--- lunar/src/lunar.c
+++ lunar/src/lunar.c
@@ -2,6 +2,7 @@
 
 #include <libgen.h>
 #include <stdbool.h>
+#include <getopt.h>
 
 int
 pmain (int argc, char *argv[])
@@ -9,30 +10,52 @@ pmain (int argc, char *argv[])
   _Bool nexfuse = false;
   _Bool openlud = false;
 
+  char *outfile = "a.out";
+
+  int optiond;
   if (argc != 3)
     {
-      printf ("usage: lunar <filename> <compiler>\n");
+      printf ("usage: %s <filename> [options]\n", argv[0]);
       return 0;
     }
 
-  char *filename = argv[1];
-  char *compiler = argv[2];
+  // Define the options
+  struct option long_options[] = { { "nexfuse", no_argument, 0, 'n' },
+                                   { "openlud", no_argument, 0, 'o' },
+                                   { "both", no_argument, 0, 'b' },
+                                   { "output", required_argument, 0, 'f' },
+                                   { 0, 0, 0, 0 } };
 
-  if (strcmp (compiler, "--nexfuse") == 0)
+  while ((optiond = getopt_long (argc, argv, "no:bf", long_options, NULL)) != -1)
     {
-      nexfuse = true;
-    }
-  else if (strcmp (compiler, "--openlud") == 0)
-    {
-      openlud = true;
-    }
-  else if (strcmp (compiler, "--both") == 0)
-    {
-      nexfuse = true;
-      openlud = true;
+      switch (optiond)
+        {
+        case 'n':
+          nexfuse = true;
+          break;
+        case 'o':
+          openlud = true;
+          break;
+        case 'b':
+          nexfuse = true;
+          openlud = true;
+          break;
+        case 'f':
+          outfile = optarg;
+          break;
+        default:
+          printf ("usage: %s <filename> [options]\n", argv[0]);
+          return 1;
+        }
     }
 
+  char *filename = argv[optind];
 
+  if (filename == NULL)
+    {
+      printf ("usage: %s <filename> [options]\n", argv[0]);
+      return 1;
+    }
 
   char c;
 
@@ -55,10 +78,17 @@ pmain (int argc, char *argv[])
   Lunar_Buffer *buf = lunar_buffer_new ();
   lunar_CG *cg = lunar_cg_init (cz);
 
-  FILE *f = fopen (filename, "r");
-    if (f == NULL)
+  if (!state || !buf || !cg)
     {
-      printf ("could not open file: `%s'\n", filename);
+      printf (
+          "lunar: could not reserve space for codegen, buffer, or state\n");
+      return -1;
+    }
+
+  FILE *f = fopen (filename, "r");
+  if (f == NULL)
+    {
+      printf ("lunar: could not open file: `%s'\n", filename);
       return -1;
     }
   while ((c = fgetc (f)) != EOF)
@@ -66,14 +96,22 @@ pmain (int argc, char *argv[])
       lunar_buffer_append (buf, c);
     }
 
-
   lunar_lex_tokenize (state, buf->ptr);
   lunar_generate (cg, state->tokens);
 
   FILE *out = fopen ("a.out", "wb");
-  fwrite (cg->code, sizeof (byte), cg->size, out);
+
+  if (out == NULL)
+    {
+      printf ("lunar: could not open file: `a.out'\n");
+      return -1;
+    }
+
+  fwrite (cg->code, sizeof (byte), cg->size * sizeof (byte), out);
+
   fclose (out);
   fclose (f);
+
   lunar_lex_free (state);
   lunar_buffer_free (buf);
   lunar_cg_free (cg);
